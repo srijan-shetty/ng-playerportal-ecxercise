cc4e38c1e421e87aa93b66d782fa171235ac5c13
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-22 15:48:42 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PlayerCardComponent } from './player-card/player-card.component';
 import { PlayerContainerComponent } from './player-container/player-container.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PlayerCardComponent,
-    PlayerContainerComponent
+    PlayerContainerComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
