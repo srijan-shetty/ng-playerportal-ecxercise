682574cecfd6e9963f7f284016ae1f7f49a1f561
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-24 12:32:55 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PlayerCardComponent } from './player-card/player-card.component';
@@ -22,7 +22,7 @@ import { RformComponent } from './rform/rform.component';
     MinlengthCloneDirective,
     RformComponent
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
