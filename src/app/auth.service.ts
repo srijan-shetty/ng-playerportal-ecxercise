ef6419726bacd74dcde14f4615c7bd2f36ee13f8
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-24 16:41:09 +0530

    done

---
+++ src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  login(formData) {
+    return this.http.post('http://localhost:3000/api/items/login', formData);
+  }
+}
