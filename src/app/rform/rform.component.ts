4dd97d79ca947efa2f989bc1927707e8fe2aab0e
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-24 15:14:52 +0530

    done

--- src/app/rform/rform.component.ts
+++ src/app/rform/rform.component.ts
@@ -9,6 +9,7 @@ import {
 } from '@angular/forms';
 import { Observable, of } from 'rxjs';
 import { ValidationService } from '../validation.service';
+import { mergeMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-rform',
@@ -40,5 +41,30 @@ export class RformComponent implements OnInit {
     return this.form.controls['name'] as FormControl;
   }
 
-  validateKey(control: AbstractControl): Observable<ValidationErrors> {}
+  validateKey(control: AbstractControl): Observable<ValidationErrors> {
+    //  get the value of the control
+    //  make a backend call
+    //  once the result is received, make decision
+    //  if validation succeeds, return null!
+    //  if validation failes, return custom error
+
+    let val = control.value;
+
+    //  start the observable by creating one from the of operator
+    //  build the pipeline by applying pipe operator
+    //  use mergeMap to make a call to the backend endpoint
+    //  use map to make the decision
+    //  return the whole chain of observables
+
+    return of(val).pipe(
+      mergeMap(v => this.validationService.validateKey(v)),
+      map(result => {
+        if (result['valid']) {
+          return null;
+        } else {
+          return { async_error: true };
+        }
+      })
+    );
+  }
 }
