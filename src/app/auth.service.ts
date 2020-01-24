ed3b21c04d1de040541551ae9e1b59c11d4d6e3a
Vivekanand P V <vivekanandpv@outlook.com>
2020-01-24 17:41:56 +0530

    done

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private loginSubject = new BehaviorSubject<boolean>(false);
+
+  isLoggedIn$ = this.loginSubject.asObservable();
+
   constructor(private http: HttpClient) {}
 
   login(formData) {
     return this.http.post('http://localhost:3000/api/items/login', formData);
   }
 
+  logout() {
+    localStorage.removeItem('app_token');
+    this.loginSubject.next(false);
+  }
+
   set token(token: string) {
     localStorage.setItem('app_token', token);
+    this.loginSubject.next(true);
   }
 }
